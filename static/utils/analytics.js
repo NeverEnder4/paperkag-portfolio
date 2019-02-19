7f39996a247ee7a76d05aa63121406b958493d5f
NeverEnder4 <apettigrew.wsdev@gmail.com>
2019-02-18 16:10:09 -0800

    enable analytics code

--- static/utils/analytics.js
+++ static/utils/analytics.js
@@ -1,18 +1,18 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  // if (!window.GA_INITIALIZED) {
-  //   ReactGA.initialize('UA-134497528-1');
-  //   window.GA_INITIALIZED = true;
-  // }
-  // logPageView();
+  if (!window.GA_INITIALIZED) {
+    ReactGA.initialize('UA-134497528-1');
+    window.GA_INITIALIZED = true;
+  }
+  logPageView();
   // console.log('GA init');
 };
 
 const logPageView = () => {
   // console.log(`Loggingpageview for ${window.location.pathname}`);
-  // ReactGA.set({ page: window.location.pathname });
-  // ReactGA.pageview(window.location.pathname);
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
 };
 
 export const logEvent = (
@@ -23,10 +23,7 @@ export const logEvent = (
   noInteraction = false,
 ) => {
   if (category && action) {
-    // ReactGA.event({ category, action, label, value, noInteraction });
-    console.log(
-      `An event of category, ${category} has happened referencing the action, ${action} of label ${label} and value of ${value}!`,
-    );
+    ReactGA.event({ category, action, label, value, noInteraction });
   }
 };
 
